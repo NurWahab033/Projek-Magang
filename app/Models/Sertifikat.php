daf15e873754147b3e9a6b8567c5698337f6e0ed
NurWahab033 <nurwahab2022@gmail.com>
2025-09-09 14:47:03 +0700

    new commit sertifikat

--- app/Models/Sertifikat.php
+++ app/Models/Sertifikat.php
@@ -19,4 +19,17 @@ class Sertifikat extends Model
     {
         return $this->belongsTo(FormulirPendaftaran::class, 'formulir_id');
     }
+
+    // 🔹 Relasi langsung ke penilaian lewat formulir (hasOneThrough)
+    public function penilaian()
+    {
+        return $this->hasOneThrough(
+            Penilaian::class,           // model tujuan
+            FormulirPendaftaran::class, // model perantara
+            'id',       // kolom foreign key di tabel formulir_pendaftaran
+            'user_id',  // kolom foreign key di tabel penilaian
+            'formulir_id', // kolom local key di tabel sertifikat
+            'user_id'   // kolom local key di tabel formulir_pendaftaran
+        );
+    }
 }
